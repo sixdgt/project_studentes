2b1c63806b3c8fb60bea02b4ba02a3ccc5a921d3
sixdgt <c4crypt@gmail.com>
2023-05-12 08:09:55 +0545

    course create, edit, show & index page created and loaded

--- project_student_enquiry_system/app_courses/urls.py
+++ project_student_enquiry_system/app_courses/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.course_index, name='course-index')
+    path('list/', views.course_index, name='course-index'),
+    path('create/', views.course_create, name='course-create'),
+    path('edit/', views.course_edit, name='course-edit'),
+    path('show/', views.course_show, name='course-show')
 ]
--- project_student_enquiry_system/app_courses/views.py
+++ project_student_enquiry_system/app_courses/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def course_index(request):
-    return render(request, 'courses/index.html')
+    context = {
+        "title": "SES | Courses", 
+        "body_title": "Here are the courses details"
+    }
+    return render(request, 'courses/index.html', context)
+
+def course_create(request):
+    return render(request, 'courses/create.html')
+
+def course_edit(request):
+    return render(request, 'courses/edit.html')
+
+def course_show(request):
+    return render(request, 'courses/show.html')
