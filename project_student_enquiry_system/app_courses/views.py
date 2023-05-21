1c166463c6d0c6183e802eed315c566f7b5fe451
sixdgt <c4crypt@gmail.com>
2023-05-21 08:04:48 +0545

    course add & list

--- project_student_enquiry_system/app_courses/views.py
+++ project_student_enquiry_system/app_courses/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app_courses.forms import CourseCreateForm
+from app_courses.models import CourseModel
+
 # Create your views here.
 def course_index(request):
+    courses = CourseModel.objects.all()
+
     context = {
         "title": "SES | Courses", 
-        "body_title": "Here are the courses details"
+        "body_title": "Here are the courses details",
+        "courses": courses
     }
     return render(request, 'courses/index.html', context)
 
 def course_create(request):
     form = CourseCreateForm()
     context = {"form": form}
+
+    if request.method == "POST":
+        name = request.POST.get("course_name")
+        code = request.POST.get("course_code")
+        cm = CourseModel(course_name=name, course_code=code)
+        cm.save()
+
+        # cm = CourseModel()
+        # cm.course_name = request.POST.get("course_name")
+        # cm.course_code = request.POST.get("course_code")
+        # cm.save()
+        return redirect("course-index")
+
     return render(request, 'courses/create.html', context)
 
 def course_edit(request):
