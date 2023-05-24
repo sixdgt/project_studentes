db83f64021557a31b577cd7b2d8285b78b7ddc7a
sixdgt <c4crypt@gmail.com>
2023-05-24 08:17:17 +0545

    student edit, update, show & delete completed

--- project_student_enquiry_system/app_students/urls.py
+++ project_student_enquiry_system/app_students/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('list/', views.student_index, name='student-index'),
     path('create/', views.student_create, name='student-create'),
     path('edit/<int:id>/', views.student_edit, name='student-edit'),
-    path('show/<int:id>/', views.student_show, name='student-show')
+    path('show/<int:id>/', views.student_show, name='student-show'),
+    path('delete/<int:id>/', views.student_delete, name='student-delete')
 ]
--- project_student_enquiry_system/app_students/views.py
+++ project_student_enquiry_system/app_students/views.py
@@ -25,8 +25,23 @@ def student_index(request):
     return render(request, "students/index.html", context)
 
 def student_edit(request, id):
-    
-    return render(request, 'students/edit.html')
+    student = StudentModel.objects.get(id=id)
+    form = StudentCreateForm(instance=student)
+    context = {"form": form}
+    if request.method == "POST":
+        std = StudentCreateForm(data=request.POST, instance=student)
+        if std.is_valid():
+            std.save()
+            return redirect("student-index")
+        return redirect("student-edit", id)
+    return render(request, 'students/edit.html', context)
 
 def student_show(request, id):
-    return render(request, 'students/show.html')
+    student = StudentModel.objects.get(id=id)
+    context = {"student": student}
+    return render(request, 'students/show.html', context)
+
+def student_delete(request, id):
+    student = StudentModel.objects.get(id=id)
+    student.delete()
+    return redirect("student-index")
