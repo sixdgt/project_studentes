332bf3a76e43eccda2f8c48f165b688ca68ea2ea
sixdgt <c4crypt@gmail.com>
2023-06-23 08:04:22 +0545

    student post completed & course post, update & delete completed - api

--- project_student_enquiry_system/app_api/serializers.py
+++ project_student_enquiry_system/app_api/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 from app_students.models import StudentModel
 from app_courses.models import CourseModel
+from django.contrib.auth.models import User
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ["course_name", "course_code"]
+        fields = ["id", "course_name", "course_code"]
         model = CourseModel
-    
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ["id", "first_name", "username"]
+        model = User
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ["first_name", "middle_name", "last_name", "email", "contact", "address", "course", "current_degree",]
+        fields = ["id", "first_name", "middle_name", "last_name", "email", "contact", "address", "course", "current_degree", "user"]
         model = StudentModel
